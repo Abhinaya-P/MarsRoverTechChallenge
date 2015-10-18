1437c775bdfed9469dedbb72b7b94f963fef4dd1
Abhinaya-P <abhinayapabsey@gmail.com>
2015-10-18 14:00:14 +0530

    Refactored Code

--- MarsRoverController.java
+++ MarsRoverController.java
@@ -1,19 +1,30 @@
-public class MarsRoverController {
-    char command;
-    String instruction;
-    MarsRover marsRover;
+import java.lang.*;
+public class MarsRoverController
+{
+	char command;
+	String instruction;
+	MarsRover rover;
+	MarsRoverController(int xCoordinate,int yCoordinate,char direction,Area area,String instr)
+	{
+		instruction = instr;
+		rover = new MarsRover(xCoordinate,yCoordinate,direction,area); //initialise the rover
+		issueCommand();
+	}
+	public void issueCommand()
+	{
+		int i;
+		for(i=0;i< instruction.length();i++)
+		{
+			command = instruction.charAt(i);
+			rover.parse(command);
+			if(rover.isValidPosition())
+				 rover.displayCurrentPosition();
+			else
+			{ 
+				System.out.println("Invalid Move!! Operation Halt!!");
+				break;
+			}
+		}
 
-    MarsRoverController(String instr, MarsRover rover) {
-        instruction = instr;
-        marsRover = rover;
-        issueCommand();
-    }
-
-    public void issueCommand() {
-        for (int i = 0; i < instruction.length(); i++) {
-            command = instruction.charAt(i);
-            marsRover.parse(command);
-        }
-
-    }
+	}
 }
